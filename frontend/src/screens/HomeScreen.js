4729d1a8e0d40cb92f12415a09573f0ed58c85fa
nadiTime <nadavcn@gmail.com>
2020-10-07 13:54:34 +0300

    Fetching Products From React (useEffect)

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,18 @@
-import React from "react";
-import products from "../products";
+import React, { useState, useEffect } from "react";
 import Product from "../components/Product";
-
+import axios from "axios";
 import { Row, Col } from "react-bootstrap";
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1>Latest Products</h1>
