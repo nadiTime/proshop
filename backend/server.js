68c2c234a949a08fd344cf74113f7bdaeebee661
nadiTime <nadavcn@gmail.com>
2020-10-08 20:15:48 +0300

    Connecting To The Database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import products from "./data/products.js";
 import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
 dotenv.config();
-
+connectDB();
 const app = express();
 
 app.get("/", (req, res) => {
