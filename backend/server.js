71c6f8598a541b54ae83a746b6388907cb3b8c02
nadiTime <nadavcn@gmail.com>
2020-10-15 00:44:31 +0300

    User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,18 @@ import connectDB from "./config/db.js";
 import colors from "colors";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 connectDB();
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("API is  running");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
