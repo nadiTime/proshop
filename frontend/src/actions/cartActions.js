7c0576cadc61f8495ead4ad0ce2a58a693ead66c
nadiTime <nadavcn@gmail.com>
2020-10-14 15:32:16 +0300

    Cart Screen

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -10,7 +10,7 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       name: data.name,
       image: data.image,
       price: data.price,
-      countInStrock: data.countInStrock,
+      countInStock: data.countInStock,
       qty
     }
   });
