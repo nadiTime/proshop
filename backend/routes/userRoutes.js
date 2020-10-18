b8a74f73833df2a7650d0df92fda22411259c4d7
nadiTime <nadavcn@gmail.com>
2020-10-18 23:42:53 +0300

    User Registration & Password Encryption

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,13 @@ import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
-import { authUser, getUserProfile } from "../controllers/userContoller.js";
+import {
+  authUser,
+  getUserProfile,
+  registerUser
+} from "../controllers/userContoller.js";
 
+router.route("/").post(registerUser);
 router.post("/login", authUser);
 router.route("/profile").get(protect, getUserProfile);
 
