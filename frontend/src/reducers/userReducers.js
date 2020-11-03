98d9425ac45e6f4211967e83dddb9776a7011197
nadiTime <nadavcn@gmail.com>
2020-11-03 13:38:57 +0200

    Profile Screen & Get User Details

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,6 +6,9 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
   USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = {}, action) => {
@@ -43,6 +46,28 @@ export const userRegisterReducer = (state = {}, action) => {
         userInfo: action.payload,
       };
     case USER_REGISTER_FAIL:
+      return {
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export const userDetailsrReducer = (state = { user: {} }, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return {
+        loading: true,
+        ...state,
+      };
+    case USER_DETAILS_SUCCESS:
+      return {
+        loading: false,
+        user: action.payload,
+      };
+    case USER_DETAILS_FAIL:
       return {
         loading: false,
         error: action.payload,
