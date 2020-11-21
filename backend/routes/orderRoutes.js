b619ac0f3c81587f83f911fa64819a2f2c185257
nadiTime <nadavcn@gmail.com>
2020-11-21 22:21:20 +0200

    Order Controller & Route

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { protect } from '../middleware/authMiddleware.js';
+const router = express.Router();
+
+import { addOrderItems } from '../controllers/orderController.js';
+
+router.route('/').post(protect, addOrderItems);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,23 @@
-import express from "express";
-import dotenv from "dotenv";
-import connectDB from "./config/db.js";
-import colors from "colors";
-import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
-import productRoutes from "./routes/productRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import colors from 'colors';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import orderRoutes from './routes/orderRoutes.js';
 dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is  running");
+app.get('/', (req, res) => {
+  res.send('API is  running');
 });
 
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
 
 app.use(notFound);
 
