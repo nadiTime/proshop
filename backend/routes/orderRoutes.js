44e96dccc912f6c311fc8120c77598b6d7acb744
nadiTime <nadavcn@gmail.com>
2020-11-25 15:37:39 +0200

    Get Order by ID Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,8 +2,8 @@ import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 
 router.route('/').post(protect, addOrderItems);
-
+router.route('/:id').get(protect, getOrderById);
 export default router;
