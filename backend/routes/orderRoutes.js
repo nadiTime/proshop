d0dfcc871a106f8b9269cbd27115d7db95e576f9
nadiTime <nadavcn@gmail.com>
2020-11-27 22:06:47 +0200

    Update To Paid Endpoint

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -2,8 +2,13 @@ import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
 const router = express.Router();
 
-import { addOrderItems, getOrderById } from '../controllers/orderController.js';
+import {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+} from '../controllers/orderController.js';
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
 export default router;
