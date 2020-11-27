a179350733120df575332ce0f149a35be5fc0cfe
nadiTime <nadavcn@gmail.com>
2020-11-27 18:54:32 +0200

    Order Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const App = () => {
   return (
@@ -20,6 +21,7 @@ const App = () => {
       <main className='py-3'>
         <Container>
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
