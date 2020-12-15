05937fed7599c14e6af4eb77b429ca5976808806
nadiTime <nadavcn@gmail.com>
2020-12-15 14:49:39 +0200

    Front End Product Reviews

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,7 @@ import {
   productDeleteReducer,
   productCreateReducer,
   productUpdateReducer,
+  productReviewCreateReducer,
 } from './reducers/productReducers';
 import { cartReducer } from './reducers/cartReducers';
 import {
@@ -47,6 +48,7 @@ const reducer = combineReducers({
   ordersList: ordersListReducer,
   productCreate: productCreateReducer,
   productUpdate: productUpdateReducer,
+  productReviewCreate: productReviewCreateReducer,
 });
 const cartItemsFromStorage = localStorage.getItem('cartItems')
   ? JSON.parse(localStorage.getItem('cartItems'))
