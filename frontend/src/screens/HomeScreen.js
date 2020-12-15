24fe29f6590ad5d6aa18709d225416f48aafc736
nadiTime <nadavcn@gmail.com>
2020-12-15 18:26:46 +0200

    Custom Page Titles & Meta

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Product from '../components/Product';
 import Message from '../components/Message';
@@ -7,6 +8,7 @@ import ProductCarousel from '../components/ProductCarousel';
 import Paginate from '../components/Paginate';
 import { Row, Col } from 'react-bootstrap';
 import { listProducts } from '../actions/productActions';
+import Meta from '../components/Meta';
 
 const HomeScreen = ({ match }) => {
   const keyword = match.params.keyword;
@@ -21,7 +23,14 @@ const HomeScreen = ({ match }) => {
   }, [dispatch, keyword, pageNumber]);
   return (
     <>
-      {!keyword && <ProductCarousel />}
+      <Meta />
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to='/' className='btn btn-light'>
+          Go back
+        </Link>
+      )}
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
